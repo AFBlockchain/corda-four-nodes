2677b04492aa2d04cc58677566c6979752cb9676
McXD <fyl155165@gmail.com>
2021-12-29 16:39:24 +0800

    Modify tests

--- containerized-test/app/src/test/java/com/example/ConnectionTest.java
+++ containerized-test/app/src/test/java/com/example/ConnectionTest.java
@@ -1,23 +1,27 @@
 package com.example;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import net.corda.client.rpc.CordaRPCClient;
 import net.corda.client.rpc.CordaRPCConnection;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.utilities.NetworkHostAndPort;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Run nodes before running this test
  */
+@RunWith(JUnitParamsRunner.class)
 public class ConnectionTest {
     Logger logger = LoggerFactory.getLogger(ConnectionTest.class);
 
     @Test
-    public void test() {
+    @Parameters({"10010", "10011", "10012", "10013"})
+    public void test(int port) {
         String host = "localhost";
-        int port = 10011;
 
         NetworkHostAndPort networkHostAndPort = new NetworkHostAndPort(host, port);
         CordaRPCClient client = new CordaRPCClient(networkHostAndPort);
--- containerized-test/app/src/test/java/com/example/DockerBasedTest.java
+++ containerized-test/app/src/test/java/com/example/DockerBasedTest.java
@@ -1,49 +1,73 @@
 package com.example;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import net.corda.client.rpc.CordaRPCClient;
 import net.corda.client.rpc.CordaRPCConnection;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.utilities.NetworkHostAndPort;
+import org.apache.commons.collections.map.HashedMap;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
+@RunWith(JUnitParamsRunner.class)
 public class DockerBasedTest {
     private static final Logger logger = LoggerFactory.getLogger(DockerBasedTest.class);
     public static final String IMAGE_NAME = "corda-four-nodes";
-    public static final String IMAGE_VERSION = "0.1.0";
+    public static final String IMAGE_VERSION = "0.1.1";
 
     @ClassRule
     @SuppressWarnings("rawtypes")
     public static GenericContainer network = new GenericContainer(DockerImageName.parse(IMAGE_NAME+":"+IMAGE_VERSION))
-            .withExposedPorts(10010)
+            .withExposedPorts(10010, 10011, 10012, 10013)
             .withStartupTimeout(Duration.ofSeconds(180));
 
     private static String host;
-    private static Integer port_n;
+    private static Map<String, Integer> portMap;
 
     @Before
     public void setUp() {
         host = network.getHost();
-        port_n = network.getMappedPort(10010);
+        portMap = new HashMap<>();
+
+        portMap.put("Notary", 10010);
+        portMap.put("PartyA", 10011);
+        portMap.put("PartyB", 10012);
+        portMap.put("PartyC", 10013);
     }
 
     @Test
-    public void notaryConnectionTest() {
-        NetworkHostAndPort networkHostAndPort = new NetworkHostAndPort(host, port_n);
-        logger.info("Address: " + networkHostAndPort);
+    @Parameters({"Notary", "PartyA", "PartyB", "PartyC"})
+    public void canConnectToNodes(String name) {
+        logger.info("Connecting to " + name);
 
+        int port = portMap.get(name);
+        CordaRPCOps proxy = getProxy(port);
+
+        String nodeLegalName = proxy.nodeInfo().getLegalIdentities().get(0).getName().toString();
+        logger.info("Legal name: " + nodeLegalName);
+
+        assert  nodeLegalName.contains(name);
+    }
+
+
+    private CordaRPCOps getProxy(int port) {
+        NetworkHostAndPort networkHostAndPort = new NetworkHostAndPort(host, port);
         CordaRPCClient client = new CordaRPCClient(networkHostAndPort);
         CordaRPCConnection connection = client.start("user1", "test");
-        CordaRPCOps proxy = connection.getProxy();
 
-        logger.info(proxy.nodeInfo().toString());
+        return connection.getProxy();
     }
 }
